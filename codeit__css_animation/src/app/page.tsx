6fb9fcb72d15cfdf180106f94e146d3f1bb7cef8
jadewisemann <jadewisemann@gmail.com>
2025-04-29 09:38:15 +0900

    framer motion 간단한 실습 - 문제 3)

--- codeit__css_animation/src/app/page.tsx
+++ codeit__css_animation/src/app/page.tsx
@@ -2,12 +2,21 @@ import * as motion from "motion/react-client"
 
 export default function Home() {
   return (
-    <motion.button
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-      className="rounded-md border-none bg-blue-500 px-5 py-2.5 text-white"
+    <motion.div
+      initial = {{
+        opacity: 0,
+        y: 20
+      }}
+      animate = {{
+        opacity: 1,
+        y: 0
+      }}
+      transition = {{
+        duration: 0.5,
+        ease: "easeInOut"
+      }}
+      className="h-96 w-72 rounded-lg border shadow-md"
     >
-      마우스를 올려보세요!
-    </motion.button>
+    </motion.div>
   )
 }
